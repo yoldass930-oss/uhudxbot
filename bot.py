b2aa8df4ee9d476c225600d4d4297c531d30c3c1
yoldass930-oss <yoldass930@gmail.com>
2026-02-14 04:55:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,22 +6,11 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "Selam aleyküm babuş buranın yakışıklı güvenliği benim bir sıkıntın oldumu etiket atman yeterli")
+    bot.reply_to(message, "Selam aleyküm güvemlik ve asayişten ben sorumluyum😏")
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     if "@uhudxbot" in message.text.lower():
-        bot.reply_to(message, "Beni mi çağırdın kral sorun nedir")
-
-bot.infinity_polling()
-import random
-
-@bot.message_handler(func=lambda message: bot.get_me().username.lower() in message.text.lower())
-def mention_handler(message):
-    cevaplar = [
-        "Beni mi çağırdın lan ",
-        "Buradayım kral ",
-        "dinliyorum😒 ",
-        "Ne var la"
-    ]
-    bot.reply_to(message, random.choice(cevaplar))
+        bot.reply_to(message, "Beni mi çağırdın kral sorun nedir"
+                    "seni dinliyorum"
+                     "ne var la"
